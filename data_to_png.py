b6484de49132ea3cedddda697766341b6b2d2e9e
Joshua Gagnon <josh.n.gagnon@gmail.com>
2017-10-02 09:21:30 +1300

    iamges now transparent

--- data_to_png.py
+++ data_to_png.py
@@ -7,13 +7,14 @@ def string_to_png(string, output):
     string = string.encode('utf-16be')
     while len(string) % 3 != 0:
         string += ' '.encode('utf-16be')
-    # make every fourth byte 0
-    string = reduce(lambda x, y: x + list(y) + [0], list(zip(*[iter(string)]*3)), [])
+    # make every 4th byte 0
+    string = reduce(lambda x, y: x + list(y) + [0], list(zip(*[iter(string)] * 3)), [])
     png.from_array([string], 'RGBA').save(output)
 
 def png_to_string(file):
     pixels = png.Reader(file).read_flat()[2]
-    pixels = reduce(lambda x, y: x + list(y)[:3], list(zip(*[iter(pixels)]*4)), [])
+    # remove every 4th byte
+    pixels = reduce(lambda x, y: x + list(y)[:3], list(zip(*[iter(pixels)] * 4)), [])
     return bytearray(pixels).decode('utf-16be')
 
 
